fbdaf394fb8f321b85053644aac208b3dd64610a
ukrnik <152810933+ukrnik@users.noreply.github.com>
2025-04-10 01:06:22 +0000

    Deploying to gh-pages from @ ukrnik/goit-js-hw-10@37079ca7f66584a3f70dbba80a4f63c5345ab74f 🚀

---
+++ 1-timer.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{f as s}from"./assets/vendor-DOPLpFvu.js";s("#datetime-picker",{dateFormat:"Y-m-d"});function t(o){const n=Math.floor(o/864e5),r=Math.floor(o%864e5/36e5),c=Math.floor(o%864e5%36e5/6e4),e=Math.floor(o%864e5%36e5%6e4/1e3);return{days:n,hours:r,minutes:c,seconds:e}}console.log(t(2e3));console.log(t(14e4));console.log(t(2414e4));
+//# sourceMappingURL=1-timer.js.map
